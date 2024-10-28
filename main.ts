59d17584c7d08444acb5ba9ee7569e6e5651e1be
adriasilgado <146826970+adriasilgado@users.noreply.github.com>
2024-10-28 16:18:34 +0100

    Els 2 reptes funcionen

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
 basic.forever(function () {
-	
+    if (input.temperature() > 22) {
+        basic.showString("Calor")
+    } else {
+        basic.showString("Fred")
+    }
+})
+basic.forever(function () {
+    if (input.lightLevel() > 200) {
+        music.play(music.stringPlayable("C5 G B A F A C5 B ", 120), music.PlaybackMode.UntilDone)
+    } else if (input.lightLevel() < 50) {
+        music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.UntilDone)
+    } else {
+    	
+    }
 })
